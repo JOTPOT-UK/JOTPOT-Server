0de28eba9ed00469bca88da76632c3e73542a700
Jacob O'Toole <jacob.otoole1@gmail.com>
2017-01-19 05:52:52 +0000

    Get logs via localhost

--- getlogs.js
+++ getlogs.js
@@ -1,5 +1,5 @@
 let net = require("net") ;
-let s = net.connect({host:"192.168.1.11",port:500}) ;
+let s = net.connect({host:"localhost",port:500}) ;
 s.on("data",d=>{
 	
 	console.log(d.toString()) ;
