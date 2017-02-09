4f58691ae01c4bbd16279a365edcf4229eed9026
Jacob O'Toole <jacob.otoole1@gmail.com>
2017-02-09 06:49:33 +0000

    Commented all the new externals code

--- externals.js
+++ externals.js
@@ -8,15 +8,19 @@
 //Version 1.0.1
 
 
+//Node modules
 let fs = require("fs") ;
 let vm = require("vm") ;
 let events = require("events") ;
 
 let handles = new Object() ;
 let handleOver = new Object() ;
+
 module.exports.handles = handles ;
+
 function handle(evt,func,allowOverwrite=true) {
 	
+	//New handles allowed?
 	if (typeof handleOver[evt] === "undefined") {
 		
 		handleOver[evt] = allowOverwrite ;
@@ -29,60 +33,76 @@ function handle(evt,func,allowOverwrite=true) {
 		
 	}
 	
+	
+	//If there are no handles yet for the event.
 	if (typeof handles[evt] === "undefined") {
 		
+		//Set this as the only handle
 		handles[evt] = [func] ;
 		
 	}
 	
 	else {
 		
+		//Or add this as a handle
 		handles[evt].push(func) ;
 		
 	}
 	
-	//handles[evt] = func ;
+	//YAY Handle set!!!
 	return true ;
 	
 }
 
 module.exports.doEvt = function (evt,...args) {
 	
+	//If the event has no handles.
 	if (typeof handles[evt] === "undefined") {
 		
+		//Carry on
 		return false ;
 		
 	}
 	
+	//Will return false if nothing returns true.
 	let rv = false ;
+	
+	//Go through all the handles for the event.
 	for (let doing in handles[evt]) {
 		
+		//If it returns true
 		if (handles[evt][doing](...args)) {
 			
+			//Then we should return true... But later
 			rv = true ;
 			
 		}
 		
 	}
+	//Return what we need to return.
 	return rv ;
 	
 }
 
+//Set up an event emmitor for recieving variable stuff.
 let varEvt = new events() ;
 process.on("message",m=>{
 	
+	//Got a var
 	if (m[0] === "gv") {
 		
 		varEvt.emit("got " + m[1],m[2]) ;
 		
 	}
 	
+	//Set a var
 	else if (m[0] === "sv") {
 		
 		varEvt.emit("set " + m[1]) ;
 		
 	}
 	
+	//Var no exist
 	else if (m[0] === "fv") {
 		
 		varEvt.emit("got " + m[1],void(0)) ;
@@ -91,60 +111,82 @@ process.on("message",m=>{
 	
 }) ;
 
+//Function to load an extention. File is path to extention, and serverObj is the object that will be the base of the server variable.
 module.exports.loadExt = (file,serverObj) => {
 	
+	//If the extention doesn't exist...
 	if (!fs.existsSync(file)) {
 		
+		//We don't load with the error "Not found"
 		return {"loaded":false,"error":"Not found"} ;
 		
 	}
 	
+	//Ah, but if it isn't a file:
 	if (!fs.statSync(file).isFile()) {
 		
+		//We don't load with the error "Not file"
 		return {"loaded":false,"error":"Not file"} ;
 		
 	}
 	
+	//Add the handle function to the serverObj
 	serverObj.handle = handle ;
+	
+	//Functiom to set a global variable. 1 arg is var name to get.
 	serverObj.getGlobal = varTG => {
 		
 		return new Promise(resolve=>{
 			
+			//When we get the variable.
 			varEvt.once("got " + varTG,d=>{
 				
+				//Resolve with the contents
 				resolve(d) ;
 				
 			});
+			
+			//Send a request to the master process to get the variable.
 			process.send(["gv",varTG]) ;
 			
 		}) ;
 		
 	} ;
 	
+	//Function to set a variable. 1st arg is var name, second is value to set it to.
 	serverObj.setGlobal = (varTS,val) => {
 		
 		return new Promise(resolve=>{
 			
+			//When it is set, resolve
 			varEvt.once("set " + varTS,_=>resolve()) ;
+			//Tell the master to set it.
 			process.send(["sv",varTS,val]) ;
 			
 		}) ;
 		
 	} ;
 	
+	//Wrap the source
 	let source = `(function(require,server,console,setTimeout,setInterval){${fs.readFileSync(file).toString()}});` ;
+	
+	//To catch so ther server doesn't go down if the extention fails to load.
 	try {
 		
+		//Run it with the required arguments
 		vm.runInNewContext(source,{},{
 			
 			filename: file + "fun"
 			
 		})(require,serverObj,console,setTimeout,setInterval) ;
+		
+		//So, it loaded...
 		return {"loaded":true,"serverObj":serverObj} ;
 		
 	}
 	catch(err) {
 		
+		//Somthing went wrong:
 		return {"loaded":false,"error":err} ;
 		
 	}
