b6d8c8334c9d0348a7b6f93c624b5a029d098b2e
Jacob O'Toole <jacob.otoole1@gmail.com>
2017-05-12 17:36:46 +0100

    Added icon and build script. Added request stamping in accounts.js. Fixed bugs in multipart-form-data-parser.js. And finally added licence to the headers of all the files and bumped the version up to 25C

--- accounts.js
+++ accounts.js
@@ -1,15 +1,35 @@
 /*
 	
-	JOTPOT Server.
-	Copyright (c) Jacob O'Toole 2016-2017
+	JOTPOT Server
+	Version 25C
+	
+	Copyright (c) 2016-2017 Jacob O'Toole
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
 	
 */
-
-//Version 25A
 
 
 let fs = require("fs") ;
 let Events = require("events") ;
+
+let stamp = 0 ;
 
 //Create the emitter.
 class NewEmitter extends Events {}
@@ -23,7 +43,7 @@ process.on("message",toDo=>{
 	
 	if (toDo[0] === "proc-authed") {
 		
-		procUpdate.emit(`authed-${toDo[1]}-${toDo[2]}`,toDo[3]) ;
+		procUpdate.emit(`authed-${toDo[1]}-${toDo[2]}-${toDo[4]}`,toDo[3]) ;
 		
 	}
 	
@@ -375,14 +395,15 @@ class proc {
 	
 	isAuthed (user) {
 		
+		let thisStamp = stamp++ ;
 		return new Promise((resolve,reject) => {
 			
-			procUpdate.once(`authed-${this.ID}-${user}`,rv=>{
+			procUpdate.once(`authed-${this.ID}-${user}-${thisStamp}`,rv=>{
 				
 				resolve(rv) ;
 				
 			}) ;
-			process.send(["proc","authed",this.ID,user]) ;
+			process.send(["proc","authed",this.ID,user,thisStamp]) ;
 			
 		}) ;
 		
---
+++ build/build.js
@@ -0,0 +1,56 @@
+const p = "J:\\JOTPOT-Server" ;
+
+const fs = require("fs") ;
+const path = require("path") ;
+const cp = require("child_process") ;
+fs.mkdirSync("build") ;
+process.chdir("build") ;
+function make(dir,...spawnArgs) {
+	
+	const startDir = process.cwd() ;
+	fs.mkdirSync(dir) ;
+	process.chdir(dir) ;
+	cp.execSync(`bash -c "wget https://nodejs.org/dist/v6.10.2/node-v6.10.2.tar.gz"`) ;
+	cp.execSync(`bash -c "tar -xzf node-v6.10.2.tar.gz"`) ;
+	cp.execSync(`bash -c "rm node-v6.10.2.tar.gz"`) ;
+	cp.execSync(`bash -c "mv ./node-v6.10.2 ./build"`) ;
+	process.chdir("build") ;
+	fs.mkdirSync("./lib/internal/jps") ;
+	fs.writeFileSync("./lib/internal/jps/run.js",sortPaths(fs.readFileSync(path.join(p,"run.js")))) ;
+	//let mimes = JSON.parse(fs.readFileSync("./mimes.dat").toString()) ;
+	fs.writeFileSync("./lib/internal/jps/server.js",sortPaths(fs.readFileSync(path.join(p,"server.js"))).replace(/let mimes = JSON\.parse\(fs\.readFileSync\(.*\)\.toString\(\)\) ;/g,`let mimes = ${fs.readFileSync(path.join(p,"mimes.dat"))} ;`)) ;
+	fs.writeFileSync("./lib/internal/jps/externals.js",sortPaths(fs.readFileSync(path.join(p,"externals.js")))) ;
+	fs.writeFileSync("./lib/internal/jps/accounts.js",sortPaths(fs.readFileSync(path.join(p,"accounts.js")))) ;
+	fs.writeFileSync("./src/res/node.rc",fs.readFileSync("./src/res/node.rc").toString()
+																						.replace(/Node\.js: Server-side JavaScript/g,"JOTPOT Server")
+																						.replace(/VALUE "ProductName", ".*"/g,`VALUE "ProductName", "JOTPOT Server"`)
+																						.replace(/VALUE "CompanyName", ".*"/g,`VALUE "CompanyName", "JOTPOT"`)
+																						.replace(/VALUE "OriginalFilename", ".*"/g,`VALUE "OriginalFilename", "jps.exe"`)
+																						.replace(/VALUE "LegalCopyright", ".*"/g,`VALUE "LegalCopyright", "JOTPOT Server Copyright Jacob O'Toole. MIT Licence. Node.js Copyright Node.js contributors. MIT license."`)
+																						.replace(/Node\.js: Server-side JavaScript/g,"JOTPOT Server")
+																						.replace(/node\.ico/g,"jps.ico")) ;
+	fs.writeFileSync("./src/res/jps.ico",fs.readFileSync(path.join(p,"jps.ico"))) ;
+	fs.writeFileSync("./node.gyp",fs.readFileSync("./node.gyp").toString().replace(/      'lib\/internal\/bootstrap_node\.js',/g,"      'lib/internal/bootstrap_node.js',\n      'lib/internal/jps/run.js',\n      'lib/internal/jps/server.js',\n      'lib/internal/jps/externals.js',\n      'lib/internal/jps/accounts.js',")) ;
+	fs.unlinkSync("./src/res/node.ico") ;
+	fs.writeFileSync("./lib/internal/bootstrap_node.js",fs.readFileSync("./lib/internal/bootstrap_node.js").toString().replace(/if \(NativeModule.exists\('_third_party_main'\)\) {/g,"if (true) {if (process.argv[1] && process.env.NODE_UNIQUE_ID) {const cluster=NativeModule.require('cluster');cluster._setupWorker();delete process.env.NODE_UNIQUE_ID;}process.nextTick(function(){NativeModule.require('internal/jps/run');})} else if (NativeModule.exists('_third_party_main')) {")) ;
+	cp.spawnSync(spawnArgs[0],spawnArgs[1],{stdio:"inherit"}) ;
+	process.chdir(startDir) ;
+	
+}
+
+const sortPaths = c => c.toString().replace(/\.\/accounts.js/gi,"internal/jps/accounts").replace(/\.\/externals.js/gi,"internal/jps/externals").replace(/\.\/server.js/gi,"internal/jps/server").replace(/\.\/run.js/gi,"internal/jps/run") ;
+
+make("win-x64",".\\vcbuild.bat",["x64"]) ;
+make("win-x86",".\\vcbuild.bat",["x86"]) ;
+make("linux-x64","bash",["-c","./configure --dest-cpu x64 && make -j4"]) ;
+make("linux-x86","bash",["-c","./configure --dest-cpu x86 && make -j4"]) ;
+make("linux-arm","bash",["-c","./configure --dest-cpu arm && make -j4"]) ;
+make("linux-arm64","bash",["-c","./configure --dest-cpu arm64 && make -j4"]) ;
+
+fs.mkdirSync("../Release") ;
+fs.writeFileSync("../Release/jps-win-x64.exe",fs.readFileSync("./win-x64/build/Release/node.exe")) ;
+fs.writeFileSync("../Release/jps-win-x86.exe",fs.readFileSync("./win-x86/build/Release/node.exe")) ;
+fs.writeFileSync("../Release/jps-linux-x64",fs.readFileSync("./linux-x64/build/out/Release/node")) ;
+fs.writeFileSync("../Release/jps-linux-x86",fs.readFileSync("./linux-x86/build/out/Release/node")) ;
+fs.writeFileSync("../Release/jps-linux-arm",fs.readFileSync("./linux-arm/build/out/Release/node")) ;
+fs.writeFileSync("../Release/jps-linux-arm64",fs.readFileSync("./linux-arm64/build/out/Release/node")) ;
